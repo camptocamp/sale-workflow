873b08e8e8eba85615884b1a65e766455b487d81
Andrius Laukavičius <andrius@focusate.eu>
2020-11-24 17:38:43 +0200

    [IMP] sale_coupon_multi_use: m2m relation
    `coupon_multi_use_ids` on sale.order is now used to track multi-use
    coupons on sale orders. `applied_coupon_ids` field did not suit multi-use
    coupons case, because it is o2m field where if same coupon is applied
    on next sale order, its relation with old sale order is removed.
    
    This relation follows similar logis as is for `applied_coupon_ids` field
    relation logic.

--- sale_coupon_multi_use/models/sale_order.py
+++ sale_coupon_multi_use/models/sale_order.py
@@ -1,4 +1,10 @@
-from odoo import models
+from odoo import fields, models
+
+ORDER_CTX_KEY = "coupon_sale_order"
+
+
+def _get_order_line_coupons_filter(line):
+    return lambda r: r.program_id.discount_line_product_id == line.product_id
 
 
 class SaleOrder(models.Model):
@@ -6,27 +12,123 @@ class SaleOrder(models.Model):
 
     _inherit = "sale.order"
 
-    # TODO: implement multi-use coupon relation on sale.order ->
-    # applied_coupon_ids is o2m, which removes coupon if same coupon
-    # is applied on another SO. But for multi-use coupon, it must be
-    # able to keep same coupon on multiple sale orders.
-    # TODO: action_draft seems buggy, because it does not reset back
-    # coupons to new state (you can abuse coupon this way and use normal
-    # coupon multiple times).
+    coupon_multi_use_ids = fields.Many2many(
+        "sale.coupon",
+        "sale_order_coupon_multi_rel",
+        "sale_id",
+        "coupon_id",
+        string="Multi Use Coupons",
+        copy=False,
+        readonly=True,
+    )
+
+    def _get_multi_use_coupons_to_handle(self, filter_func=None):
+        self.ensure_one()
+        coupons = self.coupon_multi_use_ids - self.applied_coupon_ids
+        if filter_func:
+            return filter_func(coupons)
+        return coupons
+
+    def _trigger_multi_use_coupons(self, state, filter_func=None):
+        self.ensure_one()
+        # NOTE. This method must be called with ORDER_CTX_KEY on order.
+        multi_use_coupons = self._get_multi_use_coupons_to_handle()
+        if multi_use_coupons:
+            multi_use_coupons.write({"state": state})
+        return multi_use_coupons
 
     def action_confirm(self):
         """Extend to pass coupon_sale_order context."""
         for order in self:
-            order = order.with_context(coupon_sale_order=order)
+            # Mimic same behavior as for single-user coupon.
+            order = order.with_context(**{ORDER_CTX_KEY: order})
             super(SaleOrder, order).action_confirm()
+            order._trigger_multi_use_coupons("used")
         return True
 
     def action_cancel(self):
         """Extend to pass coupon_sale_order context."""
-        # NOTE. Can't rely on coupon/SO relation, because
-        # applied_coupon_ids is o2m field, meaning same coupon used on
-        # another SO, would make lose relation on previous SO.
         for order in self:
-            order = order.with_context(coupon_sale_order=order)
+            order = order = order.with_context(**{ORDER_CTX_KEY: order})
             super(SaleOrder, order).action_cancel()
+            order._trigger_multi_use_coupons("new")
         return True
+
+    def action_draft(self):
+        """Extend to pass coupon_sale_order context."""
+        super().action_draft()
+        for order in self:
+            # TODO: refactor this to use same approach as for
+            # action_confirm and action_cancel if Odoo fixes their bug.
+            # Now single use coupons are not reset back if you put SO to
+            # draft state.
+            coupons_multi_use = order.coupon_multi_use_ids
+            if coupons_multi_use:
+                coupons_multi_use.with_context(**{ORDER_CTX_KEY: order}).write(
+                    {"state": "used"}
+                )
+        return True
+
+    def _get_coupons_from_2many_commands(self, commands):
+        ids = []
+        for cmd in commands:
+            if cmd[0] == 6:
+                ids.extend(cmd[2])
+            elif cmd[0] == 4:
+                ids.append(cmd[1])
+        return self.env["sale.coupon"].browse(ids)
+
+    def _get_multi_use_coupons_from_2many_commands(self, commands):
+        coupons = self._get_coupons_from_2many_commands(commands)
+        return coupons.filtered("multi_use")
+
+    def write(self, vals):
+        """Extend to add multi-use coupons."""
+        if vals.get("applied_coupon_ids"):
+            coupons_multi_use = self._get_multi_use_coupons_from_2many_commands(
+                vals["applied_coupon_ids"]
+            )
+            if coupons_multi_use:
+                vals["coupon_multi_use_ids"] = [(6, 0, coupons_multi_use.ids)]
+        return super().write(vals)
+
+
+class SaleOrderLine(models.Model):
+    """Extend to reverse relate with consumption line."""
+
+    _inherit = "sale.order.line"
+
+    def unlink(self):
+        """Extend to reactivate/reset removed multi-use coupons."""
+        related_program_lines = self.env["sale.order.line"]
+        # Reactivate coupons related to unlinked reward line
+        for line in self.filtered(lambda line: line.is_reward_line):
+            order = line.order_id
+            _filter = _get_order_line_coupons_filter(line)
+            coupons_to_detach = order.coupon_multi_use_ids.filtered(_filter)
+            # We can only reactivate coupons that are not part of
+            # applied_coupon_ids, because that part will signal
+            # reactivate too.
+            coupons_to_reactivate = (
+                coupons_to_detach - order.applied_coupon_ids.filtered(_filter)
+            )
+            coupons_to_reactivate.write({"state": "new"})
+            order.coupon_multi_use_ids -= coupons_to_detach
+            # Remove the program from the order if the deleted line is
+            # the reward line of the program.
+            # And delete the other lines from this program (It's the
+            # case when discount is split per different taxes)
+            related_program = self.env["sale.coupon.program"].search(
+                [("discount_line_product_id", "=", line.product_id.id)]
+            )
+            if related_program:
+                # No need to remove promotions, because multi-use
+                # coupons are not related with promotions.
+                related_program_lines |= (
+                    order.order_line.filtered(
+                        lambda l: l.product_id
+                        == related_program.discount_line_product_id
+                    )
+                    - line
+                )
+        return super(SaleOrderLine, self | related_program_lines).unlink()
