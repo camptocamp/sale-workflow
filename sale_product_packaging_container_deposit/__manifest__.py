6af70f7462ad51c560552f8e9ed7ebc6a900176f
Telmo Santos <telmo.santos@camptocamp.com>
2023-09-21 15:35:17 +0200

    [ADD] sale_product_packaging_container_deposit

---
+++ sale_product_packaging_container_deposit/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Camptocamp (<https://www.camptocamp.com>).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
+
+{
+    "name": "Sale Product Packaging Container Deposit",
+    "version": "16.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Product",
+    "summary": "Sale Product Packaging Container Deposit",
+    "author": "Camptocamp, BCIM, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "license": "LGPL-3",
+    "depends": ["sale", "product_packaging_container_deposit"],
+    "data": [],
+    "installable": True,
+    "auto_install": False,
+}
