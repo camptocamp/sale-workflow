a8ab81cfb010c28ed681f5d165346c9c25eb48a9
Akim Juillerat <akim.juillerat@camptocamp.com>
2024-06-18 20:38:33 +0200

    Add module sale_order_split_strategy_delivery

---
+++ sale_order_split_strategy_delivery/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+{
+    "name": "Sale Order Split Strategy Delivery",
+    "summary": "Define strategies to split sales orders",
+    "version": "14.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Sales Management",
+    "website": "https://github.com/OCA/sale-workflow",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "maintainers": ["grindtildeath"],
+    "license": "AGPL-3",
+    "depends": [
+        "delivery",
+        "sale_order_split_strategy",
+    ],
+    "data": [],
+    "auto_install": True,
+}
