92f6495a666926f7a54fe3d28b08ab77e3e7bf3d
Akim Juillerat <akim.juillerat@camptocamp.com>
2024-07-12 19:03:50 +0200

    fixup! fixup! fixup! Fix migration script import

--- sale_triple_discount/migrations/16.0.2.0.0/post-migrate.py
+++ sale_triple_discount/migrations/16.0.2.0.0/post-migrate.py
@@ -6,7 +6,7 @@ from openupgradelib import openupgrade
 
 @openupgrade.logging()
 def compute_discount(env):
-    env["sale.order.line"].search(
+    lines_with_discount = env["sale.order.line"].search(
         [
             "|",
             "|",
@@ -15,7 +15,7 @@ def compute_discount(env):
             ("discount3", "!=", 0),
         ]
     )
-    env["sale.order.line"]._compute_discount()
+    lines_with_discount._compute_discount_consolidated()
 
 
 @openupgrade.migrate()
