2cfad2b3a3bb86b3fe5f089ea53a8c4b072c2a8e
Rafael Lima <rafael.lima@camptocamp.com>
2025-07-28 17:04:54 +0200

    [IMP] sale_exception: sale_exception_show_popup configurable

--- sale_exception/models/__init__.py
+++ sale_exception/models/__init__.py
@@ -2,3 +2,4 @@
 from . import exception_rule
 from . import sale_order
 from . import sale_order_line
+from . import res_company
--- sale_exception/wizard/__init__.py
+++ sale_exception/wizard/__init__.py
@@ -1,1 +1,2 @@
 from . import sale_exception_confirm
+from . import res_config_settings
