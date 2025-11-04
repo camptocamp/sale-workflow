1dcadaca0e865c676969dcf80e8c7673142e4576
Simone Orsi <simahawk@gmail.com>
2025-11-04 17:23:36 +0100

    sale_order_carrier_auto_assign: refactor carrier computation
    * use computed field instead of onchange
    * simplify logic
    
    This change introduce consistency because no matter how you set the
    carrier, we'll have the same logic.

--- sale_order_carrier_auto_assign/models/sale_order.py
+++ sale_order_carrier_auto_assign/models/sale_order.py
@@ -1,25 +1,38 @@
 # Copyright 2020 Camptocamp SA
 # Copyright 2024 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
-from odoo import api, models
+from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    @api.onchange("partner_id", "partner_shipping_id")
-    def _add_delivery_carrier_on_partner_change(self):
-        partner = self.partner_shipping_id or self.partner_id
-        if not partner:
-            return
-        if self.company_id.carrier_on_create:
-            self._set_delivery_carrier(
-                set_delivery_line=False,
-                preserve_order_carrier=False,
-            )
+    carrier_id = fields.Many2one(
+        compute="_compute_carrier_id", store=True, readonly=False
+    )
+
+    @api.depends("partner_id", "partner_shipping_id", "order_line.product_id", "state")
+    @api.depends_context("override_carrier", "carrier_from_create")
+    def _compute_carrier_id(self):
+        """
+        Automatically compute delivery carrier.
+
+        Logic:
+        - If 'override_carrier' is in context, we override the current carrier
+        - If 'carrier_from_create' is in context,
+          we only set the carrier on creation if enabled
+        - Do not change carrier if order is confirmed (state 'sale')
+        """
+        preserve_carrier = not self.env.context.get("override_carrier")
+        on_create = self.env.context.get("carrier_from_create")
+        for order in self:
+            if on_create and not order._is_auto_set_carrier_on_create():
+                continue
+            if order.state == "sale":
+                continue
+            order._set_delivery_carrier(preserve_order_carrier=preserve_carrier)
 
     def _is_auto_set_carrier_on_create(self):
-        self.ensure_one()
         return (
             self.state in ("draft", "sent")
             and self.company_id.carrier_on_create
@@ -28,29 +41,21 @@ class SaleOrder(models.Model):
 
     @api.model_create_multi
     def create(self, vals_list):
-        ctx_carrier_on_create = self.env.context.get("carrier_on_create")
-        orders = super(SaleOrder, self.with_context(carrier_on_create=True)).create(
+        return super(SaleOrder, self.with_context(carrier_from_create=True)).create(
             vals_list
         )
-        orders = orders.with_context(carrier_on_create=ctx_carrier_on_create)
-        orders._set_carrier_on_create()
-        return orders
-
-    def _set_carrier_on_create(self):
-        if self.env.context.get("carrier_on_create"):
-            return
-        for order in self:
-            if not order.carrier_id and order._is_auto_set_carrier_on_create():
-                order.with_context(carrier_on_create=True)._set_delivery_carrier()
 
     def write(self, vals):
-        # When product lines are added, set the carrier
-        res = super(SaleOrder, self.with_context(carrier_on_create=True)).write(vals)
-        self._set_carrier_on_create()
-        return res
+        if self._is_auto_set_carrier_on_write(vals):
+            self = self.with_context(override_carrier=True)
+        return super().write(vals)
+
+    def _is_auto_set_carrier_on_write(self, vals):
+        return not vals.get("carrier_id") and (
+            vals.get("partner_id") or vals.get("partner_shipping_id")
+        )
 
     def _is_auto_set_carrier_on_confirm(self):
-        self.ensure_one()
         return self.company_id.carrier_auto_assign and not self.is_all_service
 
     def action_confirm(self):
@@ -73,7 +78,7 @@ class SaleOrder(models.Model):
         for order in self:
             if not order.order_line:
                 continue
-            if order.delivery_set:
+            if order.delivery_set and preserve_order_carrier:
                 continue
             delivery_wiz_action = order.action_open_delivery_wizard()
             delivery_wiz_context = delivery_wiz_action.get("context", {})
@@ -87,9 +92,7 @@ class SaleOrder(models.Model):
                 delivery_wiz_model = delivery_wiz_model.with_context(
                     default_order_id=self._origin.id
                 )
-                delivery_wiz = delivery_wiz_model.create({})
-            else:
-                delivery_wiz = delivery_wiz_model.new({})
+            delivery_wiz = delivery_wiz_model.new({})
             # Do not override carrier
             if preserve_order_carrier and order.carrier_id:
                 delivery_wiz.carrier_id = order.carrier_id
