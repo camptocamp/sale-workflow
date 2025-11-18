02242c3cc4daa70468ab6e824fe60e95d5f76b46
Simone Orsi <simahawk@gmail.com>
2025-11-18 12:36:53 +0100

    fixup! fixup! sale_order_carrier_auto_assign: refactor carrier computation

--- sale_order_carrier_auto_assign/models/sale_order.py
+++ sale_order_carrier_auto_assign/models/sale_order.py
@@ -1,36 +1,11 @@
 # Copyright 2020 Camptocamp SA
 # Copyright 2024 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
-
-    carrier_id = fields.Many2one(
-        compute="_compute_carrier_id", store=True, readonly=False
-    )
-
-    @api.depends("partner_id", "partner_shipping_id", "order_line.product_id", "state")
-    @api.depends_context("override_carrier", "carrier_from_create")
-    def _compute_carrier_id(self):
-        """
-        Automatically compute delivery carrier.
-
-        Logic:
-        - If 'override_carrier' is in context, we override the current carrier
-        - If 'carrier_from_create' is in context,
-          we only set the carrier on creation if enabled
-        - Do not change carrier if order is confirmed (state 'sale')
-        """
-        preserve_carrier = not self.env.context.get("override_carrier")
-        on_create = self.env.context.get("carrier_from_create")
-        for order in self:
-            if on_create and not order._is_auto_set_carrier_on_create():
-                continue
-            if order.state == "sale":
-                continue
-            order._set_delivery_carrier(preserve_order_carrier=preserve_carrier)
 
     def _is_auto_set_carrier_on_create(self):
         return (
@@ -39,20 +14,30 @@ class SaleOrder(models.Model):
             and not self.is_all_service
         )
 
+    def _auto_set_carrier_on_create(self):
+        for rec in self:
+            if not rec.carrier_id and rec._is_auto_set_carrier_on_create():
+                rec._set_delivery_carrier()
+
     @api.model_create_multi
     def create(self, vals_list):
-        return super(SaleOrder, self.with_context(carrier_from_create=True)).create(
-            vals_list
-        )
+        recs = super().create(vals_list)
+        recs._auto_set_carrier_on_create()
+        return recs
 
     def write(self, vals):
+        res = super().write(vals)
         if self._is_auto_set_carrier_on_write(vals):
-            self = self.with_context(override_carrier=True)
-        return super().write(vals)
+            for rec in self:
+                if rec.state in ("draft", "sent"):
+                    rec._set_delivery_carrier(preserve_order_carrier=False)
+        return res
 
     def _is_auto_set_carrier_on_write(self, vals):
         return not vals.get("carrier_id") and (
-            vals.get("partner_id") or vals.get("partner_shipping_id")
+            vals.get("partner_id")
+            or vals.get("partner_shipping_id")
+            or vals.get("order_line")
         )
 
     def _is_auto_set_carrier_on_confirm(self):
